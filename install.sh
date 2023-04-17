b5a7b7dfbbe5f931475c2ee6f7b164fb1a497fd0
Ross McLean <ross@lykaos.com>
2023-04-17 13:17:25 +0100

    Added neovim plugin manager

--- install.sh
+++ install.sh
@@ -10,6 +10,13 @@ ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
+[! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+    && curl -fLo "$DOTFILES/nvim/autload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+
 ######
 # i3
 ######
@@ -38,3 +45,7 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 #######
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+
+# install neovim plugin manager
+# install (or update) all of the plugins
+nvim --noplugin +PlugUpdate +qa
